7044b1e67be2b9502c80b08a67f3960f573160d6
marcelonovoa <ma.novoac@duocuc.cl>
2024-05-29 10:40:37 -0400

    inicio del menu

---
+++ ejercicio_1.py
@@ -0,0 +1,21 @@
+import os
+import time
+import msvcrt
+while True:
+    print("""
+    1) inicio sesion.
+    2) Registrar usuario.
+    3) Eliminar usuario!
+    4) Salir
+    """)
+    opc = int(input("Seleccione su opcion: "))
+    if opc==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc==3:
+        pass
+    else:
+        print("adios!")
+        time.sleep
+    
